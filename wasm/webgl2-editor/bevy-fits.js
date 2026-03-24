54ba20756f37c64ecb2037a9b47d3dad2b3071b5
hlorus <64740362+hlorus@users.noreply.github.com>
2026-03-24 10:50:42 +0100

    Update WASM build

--- wasm/webgl2-editor/bevy-fits.js
+++ wasm/webgl2-editor/bevy-fits.js
@@ -1620,7 +1620,7 @@ function __wbg_get_imports() {
             return ret;
         },
         __wbindgen_cast_000000000000000a: function(arg0, arg1) {
-            // Cast intrinsic for `Closure(Closure { dtor_idx: 182207, function: Function { arguments: [Externref], shim_idx: 182208, ret: Unit, inner_ret: Some(Unit) }, mutable: true }) -> Externref`.
+            // Cast intrinsic for `Closure(Closure { dtor_idx: 182232, function: Function { arguments: [Externref], shim_idx: 182233, ret: Unit, inner_ret: Some(Unit) }, mutable: true }) -> Externref`.
             const ret = makeMutClosure(arg0, arg1, wasm.wasm_bindgen__closure__destroy__hb308c5511a25987d, wasm_bindgen__convert__closures_____invoke__h3b118b511ae5d9c3);
             return ret;
         },
--- wasm/webgpu-editor/bevy-fits.js
+++ wasm/webgpu-editor/bevy-fits.js
@@ -2595,7 +2595,7 @@ function __wbg_get_imports() {
             return ret;
         },
         __wbindgen_cast_0000000000000001: function(arg0, arg1) {
-            // Cast intrinsic for `Closure(Closure { dtor_idx: 108381, function: Function { arguments: [NamedExternref("GPUUncapturedErrorEvent")], shim_idx: 108428, ret: Unit, inner_ret: Some(Unit) }, mutable: true }) -> Externref`.
+            // Cast intrinsic for `Closure(Closure { dtor_idx: 108408, function: Function { arguments: [NamedExternref("GPUUncapturedErrorEvent")], shim_idx: 108455, ret: Unit, inner_ret: Some(Unit) }, mutable: true }) -> Externref`.
             const ret = makeMutClosure(arg0, arg1, wasm.wasm_bindgen__closure__destroy__he7455921a349fefe, wasm_bindgen__convert__closures_____invoke__ha06699b52139a2d2);
             return ret;
         },
@@ -2645,7 +2645,7 @@ function __wbg_get_imports() {
             return ret;
         },
         __wbindgen_cast_000000000000000b: function(arg0, arg1) {
-            // Cast intrinsic for `Closure(Closure { dtor_idx: 182452, function: Function { arguments: [Externref], shim_idx: 182453, ret: Unit, inner_ret: Some(Unit) }, mutable: true }) -> Externref`.
+            // Cast intrinsic for `Closure(Closure { dtor_idx: 182477, function: Function { arguments: [Externref], shim_idx: 182478, ret: Unit, inner_ret: Some(Unit) }, mutable: true }) -> Externref`.
             const ret = makeMutClosure(arg0, arg1, wasm.wasm_bindgen__closure__destroy__hb308c5511a25987d, wasm_bindgen__convert__closures_____invoke__h3b118b511ae5d9c3);
             return ret;
         },
